86e612bd9f933b210b67b84e6cff4788a8c5b27e
eunicecompra <eunicecompra@gmail.com>
2019-11-30 19:39:10 +1100

    Create side effect input from a dictionary attribute

--- phonebook_test.py
+++ phonebook_test.py
@@ -4,10 +4,17 @@ from unittest.mock import patch
 from phonebook import Phonebook
 
 class TestPhonebook(unittest.TestCase):
+    test_data = [
+        "3",
+        "sam 99912222",
+        "tom 11122222",
+        "harry 12299933"
+    ]
+
     def test_phonebook_class(self):
         self.assertIsInstance(Phonebook(), Phonebook)
 
-    @patch("builtins.input", side_effect=["3", "sam 99912222", "tom 11122222", "harry 12299933"])
+    @patch("builtins.input", side_effect=test_data)
     def test_input_details(self, input):
         p = Phonebook()
         p.readInput()
